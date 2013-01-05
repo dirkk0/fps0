eed5543fdcdd8ff5bf083de08d97dbdae61fadbe
Dirk Krause <dirkk0@googlemail.com>
2013-01-05 12:32:02 +0100

    live example

---
+++ fps0/a6.js
@@ -0,0 +1,306 @@
+{
+
+"metadata" :
+{
+	"formatVersion" : 3.2,
+	"type"          : "scene",
+	"sourceFile"    : "a6.blend",
+	"generatedBy"   : "Blender 2.65 Exporter",
+	"objects"       : 7,
+	"geometries"    : 5,
+	"materials"     : 0,
+	"textures"      : 0
+},
+
+"urlBaseType" : "relativeToScene",
+
+
+"objects" :
+{
+	"Cube.004" : {
+		"geometry"  : "geo_Cube.008",
+		"groups"    : [  ],
+		"material"  : "",
+		"position"  : [ 2.19746, 7.24545, -13.9056 ],
+		"rotation"  : [ -1.5708, 0, 0 ],
+		"quaternion": [ -0.707107, 0, 0, 0.707107 ],
+		"scale"     : [ 1, 1, 1 ],
+		"visible"       : true,
+		"castShadow"    : false,
+		"receiveShadow" : false,
+		"doubleSided"   : false
+	},
+
+	"Cube.003" : {
+		"geometry"  : "geo_Cube.007",
+		"groups"    : [  ],
+		"material"  : "",
+		"position"  : [ 3.42086, 2.08474, 2.84181 ],
+		"rotation"  : [ -1.15935, 0, 0 ],
+		"quaternion": [ -0.547753, 0, 0, 0.83664 ],
+		"scale"     : [ 1, 3.85589, 0.0834348 ],
+		"visible"       : true,
+		"castShadow"    : false,
+		"receiveShadow" : false,
+		"doubleSided"   : false
+	},
+
+	"Cube.002" : {
+		"geometry"  : "geo_Cube.006",
+		"groups"    : [  ],
+		"material"  : "",
+		"position"  : [ 0, 0.814375, -6.03795 ],
+		"rotation"  : [ -1.5708, 0, 0 ],
+		"quaternion": [ -0.707107, 0, 0, 0.707107 ],
+		"scale"     : [ 1, 1, 0.405875 ],
+		"visible"       : true,
+		"castShadow"    : false,
+		"receiveShadow" : false,
+		"doubleSided"   : false
+	},
+
+	"Cube.001" : {
+		"geometry"  : "geo_Cube.000",
+		"groups"    : [  ],
+		"material"  : "",
+		"position"  : [ -5.18563, 1.40116, 4.79598e-08 ],
+		"rotation"  : [ -1.5708, 0, 0 ],
+		"quaternion": [ -0.707107, 0, 0, 0.707107 ],
+		"scale"     : [ 1, 1, 1 ],
+		"visible"       : true,
+		"castShadow"    : false,
+		"receiveShadow" : false,
+		"doubleSided"   : false
+	},
+
+	"Cube" : {
+		"geometry"  : "geo_Cube.005",
+		"groups"    : [  ],
+		"material"  : "",
+		"position"  : [ 3.72313, 1.67944, 5.74849e-08 ],
+		"rotation"  : [ -1.5708, 0, 0 ],
+		"quaternion": [ -0.707107, 0, 0, 0.707107 ],
+		"scale"     : [ 1, 1, 1 ],
+		"visible"       : true,
+		"castShadow"    : false,
+		"receiveShadow" : false,
+		"doubleSided"   : false
+	},
+
+	"default_light" : {
+		"type"       : "DirectionalLight",
+		"direction"  : [ 0, 1, 1 ],
+		"color"      : 16777215,
+		"intensity"  : 0.80
+	},
+
+	"default_camera" : {
+		"type"  : "PerspectiveCamera",
+		"fov"   : 60.000000,
+		"aspect": 1.333000,
+		"near"  : 1.000000,
+		"far"   : 10000.000000,
+		"position": [ 0, 0, 10 ],
+		"target"  : [ 0, 0, 0 ]
+	}
+},
+
+
+"geometries" :
+{
+	"geo_Cube.008" : {
+		"type" : "embedded",
+		"id"  : "emb_Cube.008"
+	},
+
+	"geo_Cube.007" : {
+		"type" : "embedded",
+		"id"  : "emb_Cube.007"
+	},
+
+	"geo_Cube.006" : {
+		"type" : "embedded",
+		"id"  : "emb_Cube.006"
+	},
+
+	"geo_Cube.000" : {
+		"type" : "embedded",
+		"id"  : "emb_Cube.000"
+	},
+
+	"geo_Cube.005" : {
+		"type" : "embedded",
+		"id"  : "emb_Cube.005"
+	}
+},
+
+
+"embeds" :
+{
+"emb_Cube.000": {	"scale" : 1.000000,
+
+	"materials" : [	{
+		"DbgColor" : 15658734,
+		"DbgIndex" : 0,
+		"DbgName" : "default",
+		"vertexColors" : false
+	}],
+
+	"vertices" : [-1,-1,-1,-1,1,-1,1,1,-1,1,-1,-1,-1,-1,1,-1,1,1,1,1,1,1,-1,1],
+
+	"morphTargets" : [],
+
+	"normals" : [-0.577349,0.577349,-0.577349,-0.577349,-0.577349,-0.577349,-0.577349,-0.577349,0.577349,-0.577349,0.577349,0.577349,0.577349,0.577349,0.577349,0.577349,0.577349,-0.577349,0.577349,-0.577349,0.577349,0.577349,-0.577349,-0.577349],
+
+	"colors" : [],
+
+	"uvs" : [[]],
+
+	"faces" : [35,1,0,4,5,0,0,1,2,3,35,5,6,2,1,0,3,4,5,0,35,6,7,3,2,0,4,6,7,5,35,0,3,7,4,0,1,7,6,2,35,0,1,2,3,0,1,0,5,7,35,7,6,5,4,0,6,4,3,2],
+
+	"bones" : [],
+
+	"skinIndices" : [],
+
+	"skinWeights" : [],
+
+	"animation" : {}
+},
+
+"emb_Cube.006": {	"scale" : 1.000000,
+
+	"materials" : [	{
+		"DbgColor" : 15658734,
+		"DbgIndex" : 0,
+		"DbgName" : "default",
+		"vertexColors" : false
+	}],
+
+	"vertices" : [-1,-1,-1,-1,1,-1,1,1,-1,1,-1,-1,-1,-1,1,-1,1,1,1,1,1,1,-1,1],
+
+	"morphTargets" : [],
+
+	"normals" : [-0.577349,0.577349,-0.577349,-0.577349,-0.577349,-0.577349,-0.577349,-0.577349,0.577349,-0.577349,0.577349,0.577349,0.577349,0.577349,0.577349,0.577349,0.577349,-0.577349,0.577349,-0.577349,0.577349,0.577349,-0.577349,-0.577349],
+
+	"colors" : [],
+
+	"uvs" : [[]],
+
+	"faces" : [35,1,0,4,5,0,0,1,2,3,35,5,6,2,1,0,3,4,5,0,35,6,7,3,2,0,4,6,7,5,35,0,3,7,4,0,1,7,6,2,35,0,1,2,3,0,1,0,5,7,35,7,6,5,4,0,6,4,3,2],
+
+	"bones" : [],
+
+	"skinIndices" : [],
+
+	"skinWeights" : [],
+
+	"animation" : {}
+},
+
+"emb_Cube.007": {	"scale" : 1.000000,
+
+	"materials" : [	{
+		"DbgColor" : 15658734,
+		"DbgIndex" : 0,
+		"DbgName" : "default",
+		"vertexColors" : false
+	}],
+
+	"vertices" : [-1,-1,-1,-1,1,-1,1,1,-1,1,-1,-1,-1,-1,1,-1,1,1,1,1,1,1,-1,1],
+
+	"morphTargets" : [],
+
+	"normals" : [-0.577349,0.577349,-0.577349,-0.577349,-0.577349,-0.577349,-0.577349,-0.577349,0.577349,-0.577349,0.577349,0.577349,0.577349,0.577349,0.577349,0.577349,0.577349,-0.577349,0.577349,-0.577349,0.577349,0.577349,-0.577349,-0.577349],
+
+	"colors" : [],
+
+	"uvs" : [[]],
+
+	"faces" : [35,1,0,4,5,0,0,1,2,3,35,5,6,2,1,0,3,4,5,0,35,6,7,3,2,0,4,6,7,5,35,0,3,7,4,0,1,7,6,2,35,0,1,2,3,0,1,0,5,7,35,7,6,5,4,0,6,4,3,2],
+
+	"bones" : [],
+
+	"skinIndices" : [],
+
+	"skinWeights" : [],
+
+	"animation" : {}
+},
+
+"emb_Cube.008": {	"scale" : 1.000000,
+
+	"materials" : [	{
+		"DbgColor" : 15658734,
+		"DbgIndex" : 0,
+		"DbgName" : "default",
+		"vertexColors" : false
+	}],
+
+	"vertices" : [-1,-1,-1,-1,1,-1,1,1,-1,1,-1,-1,-1,-1,1,-1,1,1,1,1,1,1,-1,1],
+
+	"morphTargets" : [],
+
+	"normals" : [-0.577349,0.577349,-0.577349,-0.577349,-0.577349,-0.577349,-0.577349,-0.577349,0.577349,-0.577349,0.577349,0.577349,0.577349,0.577349,0.577349,0.577349,0.577349,-0.577349,0.577349,-0.577349,0.577349,0.577349,-0.577349,-0.577349],
+
+	"colors" : [],
+
+	"uvs" : [[]],
+
+	"faces" : [35,1,0,4,5,0,0,1,2,3,35,5,6,2,1,0,3,4,5,0,35,6,7,3,2,0,4,6,7,5,35,0,3,7,4,0,1,7,6,2,35,0,1,2,3,0,1,0,5,7,35,7,6,5,4,0,6,4,3,2],
+
+	"bones" : [],
+
+	"skinIndices" : [],
+
+	"skinWeights" : [],
+
+	"animation" : {}
+},
+
+"emb_Cube.005": {	"scale" : 1.000000,
+
+	"materials" : [	{
+		"DbgColor" : 15658734,
+		"DbgIndex" : 0,
+		"DbgName" : "default",
+		"vertexColors" : false
+	}],
+
+	"vertices" : [-1,-1,-1,-1,1,-1,1,1,-1,1,-1,-1,-1,-1,1,-1,1,1,1,1,1,1,-1,1],
+
+	"morphTargets" : [],
+
+	"normals" : [-0.577349,0.577349,-0.577349,-0.577349,-0.577349,-0.577349,-0.577349,-0.577349,0.577349,-0.577349,0.577349,0.577349,0.577349,0.577349,0.577349,0.577349,0.577349,-0.577349,0.577349,-0.577349,0.577349,0.577349,-0.577349,-0.577349],
+
+	"colors" : [],
+
+	"uvs" : [[]],
+
+	"faces" : [35,1,0,4,5,0,0,1,2,3,35,5,6,2,1,0,3,4,5,0,35,6,7,3,2,0,4,6,7,5,35,0,3,7,4,0,1,7,6,2,35,0,1,2,3,0,1,0,5,7,35,7,6,5,4,0,6,4,3,2],
+
+	"bones" : [],
+
+	"skinIndices" : [],
+
+	"skinWeights" : [],
+
+	"animation" : {}
+}
+},
+
+
+"transform" :
+{
+	"position"  : [ 0, 0, 0 ],
+	"rotation"  : [ -1.5708, 0, 0 ],
+	"scale"     : [ 1, 1, 1 ]
+},
+
+"defaults" :
+{
+	"bgcolor" : [ 0, 0, 0 ],
+	"bgalpha" : 1.000000,
+	"camera"  : "default_camera"
+}
+
+}
